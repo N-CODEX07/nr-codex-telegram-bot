49e1b7effdab7454a5a88e8cc5093ff760a88b11
NR-CODEX <kingdahariya1@gmail.com>
2025-06-18 08:35:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ define('BOT_NAME', 'NR CODEX JWT');
 define('INSTAGRAM_URL', 'https://www.instagram.com/nr_codex?igsh=MjZlZWo2cGd3bDVk');
 define('YOUTUBE_URL', 'https://youtube.com/@nr_codex06?si=5pbP9qsDLfT4uTgf');
 define('API_BASE_URLS', [
-    'https://jwt-aditya.vercel.app/token?uid={Uid}&password={Password}', // API endpoint for JWT token generation
+    'https://uditanshu-jwt-ob49.vercel.app/token?uid={uid}&password={password}, // API endpoint for JWT token generation
 ]);
 define('MAX_RETRIES', 10);
 define('CONCURRENT_REQUESTS', 55);
